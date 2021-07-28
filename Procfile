3499174073f138044d0b352fe7c1b34b920ad92a
Felipe <felipevictor67@hotmail.com>
2021-07-28 00:34:03 -0300

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn thingspeak.wsgi:application --preload --log-file - 
---
+++ thingspeakdata/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+import requests
+import json
+import datetime
+from dateutil import tz
+
+def dashboard(request):
+    json_data = requests.get('https://thingspeak.com/channels/196384/feed.json').text
+    json_loaded = json.loads(json_data)
+    local_zone = tz.gettz("America/Fortaleza")
+    context = {}
+
+    dados = json_loaded["feeds"][-500:]
+    leitura = list()
+    outdoor_temp = list()
+    temp = list()
+    air_pressure = list()
+    humidity = list()
+
+    for x in range(len(dados)):
+        date = dados[x]["created_at"]
+        value_ot = dados[x]["field1"]
+        value_t = dados[x]["field2"]
+        value_ap = dados[x]["field3"]
+        value_h = dados[x]["field4"]
+
+        formatted_date = datetime.datetime.strptime(date, "%Y-%m-%dT%H:%M:%SZ").astimezone(local_zone).strftime('%d-%m %H:%M')
+        leitura.append(formatted_date)
+        outdoor_temp.append(value_ot)
+        temp.append(value_t)
+        air_pressure.append(value_ap)
+        humidity.append(value_h)
+    
+    context = { "data" :  leitura, "outdoor": str(outdoor_temp),  "temperature" : str(temp), "air_pressure" : str(air_pressure), "humidity": str(humidity) }          
+    return render(request, 'index.html', {'contextos' : context})
