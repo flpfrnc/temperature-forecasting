b72b69cca601ecd0e260814c5bde9f457e05d62d
Felipe França <felipevictor67@hotmail.com>
2021-08-09 12:43:06 -0300

    correção settings

--- thingspeak/settings/base.py
+++ thingspeak/settings/base.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'thingspeak.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR +'/db.sqlite3',
     }
 }
 
