61eea6086f90c0c42885b87c0b5fed59ea67ca00
Felipe França <felipevictor67@hotmail.com>
2021-08-09 10:44:01 -0300

    update settings

--- thingspeak/settings/base.py
+++ thingspeak/settings/base.py
@@ -123,13 +123,15 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # also important as defining the root from compressor
 SASS_PROCESSOR_ROOT = STATIC_ROOT
